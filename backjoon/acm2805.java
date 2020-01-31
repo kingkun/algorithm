832082f0188020c8c994d441d911836c3c2743c1
quro07 <rjswnd23>
2020-01-31 20:29:27 +0900

    acm2805 pass code

--- backjoon/acm2805.java
+++ backjoon/acm2805.java
@@ -1,20 +1,25 @@
 package backjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class acm2805 {
     static public int M, N;
     static public int[] arr;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = sc.nextInt();
-        M = sc.nextInt();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         arr = new int[N];
 
+        st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++){
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
